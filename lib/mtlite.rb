bdbbf1c27972d779a0305abfcb23ba546d5b2ce4
James Robertson <james@ralina.(none)>
2014-01-29 15:50:56 +0000

    MtLite like lists are now parsed

--- lib/mtlite.rb
+++ lib/mtlite.rb
@@ -8,20 +8,41 @@ class MTLite
   
   def initialize(raw_msg)
 
+    # if it looks like an MtLite list make it an MtLite list
+    # e.g. "a todo list:\n* line 1\n* line 2" => 
+    #                                       a todo list: [* line 1 * line 2]
+    raw_msg.sub!(/^(?:[\*\[#][^\n]+\n?)+/,'[\0]')
+    raw_msg.gsub!(/\n/,' ')
+
+
+    # add br tags to checklist items
+    # todo list: [[x] line 1 [] line 2] => 
+    #                         todo list: <br/>[x] line 1 <br/>[] line 2
     raw_msg.sub!(/\[\[.*\]/){|x| x[0..-2].gsub(/[^\n](\[[x\s]?\])/,'<br/>\1') }
+
+    # convert square brackets to unicode check boxes
+    # replaces a [] with a unicode checkbox, 
+    #                         and [x] with a unicode checked checkbox
     raw_msg = raw_msg.gsub(/\[\s*\]/,'&#9744;').gsub(/\[x\]/,'&#9745;')
+
+    # convert fractions using their associated unicode character
+    # i.e. 1/4, 1/2, 3/4 
     raw_msg = raw_msg.gsub('1/4','&#188;').gsub('1/2','&#189;').gsub('3/4','&#190;')
 
+    # add strikethru to completed items
+    # e.g. -milk cow- becomes <del>milk cow</del>
     raw_msg.gsub!(/\s-[^-]+-?[^-]+-[\s\]]/) do |x|
       x.sub(/-([&\w]+.*\w+)-/,'<del>\1</del>')
     end
     
+    # append a domain label after the URL
     raw_msg.gsub!(/(?:^\[|\s\[)[^\]]+\]\((https?:\/\/[^\s]+)/) do |x|
       s2 = x[/https?:\/\/([^\/]+)/,1].split(/\./)
       r = s2.length >= 3 ? s2[1..-1] :  s2
       "%s [%s]" % [x, r.join('.')]
     end
     
+    # generate html lists from mtlite 1-liner lists
     raw_msg.gsub!(/\[[\*#][^\]]+\]/) do |list|
 
       if not list[/\n/] then
@@ -39,7 +60,9 @@ class MTLite
     end
 
     msg = RDiscount.new(raw_msg).to_html.gsub(/<\/?p[^>]*>/,'')
+    # generate anchor tags for URLs which don't have anchor tags
     msg.gsub!(/(?:^(https?:[^\s]+)|\s(https?:[^\s]+))/,' <a href="\2">\2</a>')    
+    # add the target attribute to make all hyperlinks open in a new browser tab
     msg.gsub!(/<a /,'<a target="_blank" ')
     
     @msg = msg
